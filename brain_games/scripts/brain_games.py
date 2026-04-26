3059811d2e695faf6aa30c67b3a9e7d143f1dbe7
APavlenkoDevOps <andrey.pavlenko.job@gmail.com>
2026-04-26 13:07:02 +0400

    main fie issues fixed

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,10 @@
 from brain_games.scripts.cli import welcome_user
 
 
-def start():
+def main():
     print("Welcome to the Brain Games!")
     welcome_user()
 
 
 if __name__ == "__main__":
-    start()
+    main()
