f9fe7824b732b91c94f64466089ec1767d058583
APavlenkoDevOps <andrey.pavlenko.job@gmail.com>
2026-04-26 12:49:36 +0400

    add ruff

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,10 @@
 from brain_games.scripts.cli import welcome_user
 
 
-def main():
+def start():
     print("Welcome to the Brain Games!")
     welcome_user()
 
-if  __name__  ==  "__main__":
-    main()
+
+if __name__ == "__main__":
+    start()
